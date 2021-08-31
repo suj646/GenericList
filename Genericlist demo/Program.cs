a8fa25fe803e9a8c8fc1a9e5731e7093cebc37a9
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:54:25 +0530

    GenericList

---
+++ Genericlist demo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genericlist_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<string> myList = new List<string>();
+            myList.Add("Pankaj");
+            myList.Add("vishal");
+            myList.Add("sujit");
+
+
+            foreach (string s in myList)
+                Console.Write (s.ToString() + " ");
+
+            
+            myList.Sort();
+            Console.WriteLine("\n After Sorting");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+
+            
+            myList.Remove("Pankaj");
+            Console.WriteLine("\nRemoving Pankaj");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+
+           
+            myList.Insert(2, "Pankaj");
+            Console.WriteLine("\nInserting Pankaj at index position 2");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+        }
+    }
+}
+    
